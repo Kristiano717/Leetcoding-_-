6684a4d867df1394add5d8e47dad6a2fe5d31a48
Krish Harsola <krishharsola717@gmail.com>
2026-09-19 14:10:18 +0530

    Time: 4 ms (59.87%) | Memory: 14.3 MB (68.47%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,19 +1,53 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-         long long  ans =0;
-        for ( int i =0;i<nums.size();i++)
-        {
-            int maxi = nums[i];
-             int mini = nums[i];
+        int n = nums.size();
+        long long ans = 0;
+        stack<int> st;
 
-            for( int j =i ; j< nums.size();j++)
-            {
-                maxi = max(maxi , nums[j] );
-                mini = min( mini , nums[j]);
-                ans += (maxi-mini);
+        for (int i = 0; i <= n; i++) // calucaslting for the  minimums.
+        {
+            while (!st.empty() && (i == n || nums[st.top()] > nums[i])) {
+                int j = st.top();
+                st.pop();
+
+                int k;
+                if (st.empty()) {
+                    k = -1;
+                } else {
+                    k = st.top();
+                }
+                long long righthandside = i - j;
+                long long lefthandside = j - k;
+                long long count = lefthandside * righthandside;
+
+                ans = ans - (long long)nums[j] * count;
             }
+            st.push(i);
         }
-       return ans;
+        st = stack<int>();
+        for (int i = 0; i <= n; i++) // calucaslting for the  minimums.
+        {
+            while (!st.empty() && (i == n || nums[st.top()] < nums[i])) {
+                int j = st.top();
+                st.pop();
+
+                int k;
+                if (st.empty()) {
+                     k = -1;
+                } else {
+                    k = st.top();
+                }
+                long long righthandside = i - j;
+                long long lefthandside = j - k;
+                long long count = lefthandside * righthandside;
+
+                ans = ans + (long long)nums[j] * count;
+            }
+            st.push(i);
+        }
+        return ans;
     }
-};
+
+    
+    };
