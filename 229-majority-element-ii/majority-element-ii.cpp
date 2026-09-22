2a7d19c6542dd9c1e6aef089214a0883e081fd90
Krish Harsola <krishharsola717@gmail.com>
2026-09-22 15:07:19 +0530

    Time: 2310 ms (5.20%) | Memory: 23.9 MB (41.97%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+
+            // skip if this value is already in the answer
+            if (ans.size() == 0 || ans[0] != nums[i]) {
+
+                int count = 0;
+                for (int j = 0; j < n; j++) {
+                    if (nums[j] == nums[i]) {
+                        count++;
+                    }
+                }
+
+                if (count > n / 3) {
+                    ans.push_back(nums[i]);
+                }
+            }
+
+            // at most 2 answers can exist
+            if (ans.size() == 2) {
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
